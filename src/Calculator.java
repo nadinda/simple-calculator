bb350bbe9c418592db371970a763cafeb71256a9
dinda <dinda.rachmat@gmail.com>
2018-03-12 22:00:03 +0700

    initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Calculator {
+    int number1;
+    int number2;
+    int operator;
+    double jawaban;
+
+    public Calculator() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan Bilangan Pertama : ");
+        number1 = scanner.nextInt();
+
+        System.out.print("Masukkan Bilangan Kedua : ");
+        number2 = scanner.nextInt();
+
+        System.out.println("1. Tambah ");
+        System.out.println("2. Kurang ");
+        System.out.println("3. Kali ");
+        System.out.println("1. Bagi ");
+
+        System.out.println("Pilih Operator : " );
+        operator = scanner.nextInt();
+
+        calculatorSwitchCase();
+
+    }
+
+    public void calculatorSwitchCase() {
+        switch (operator) {
+            case 1:
+                jawaban = number1 + number2;
+                break;
+            case 2:
+                jawaban = number1 - number2;
+                break;
+            case 3:
+                jawaban = number1 * number2;
+                break;
+            case 4:
+                jawaban = number1 / number2;
+                break;
+        }
+        System.out.print(number1+" "+operator+" "+number2+" = "+jawaban);
+    }
+}
