46f0a7f0af94268269ccff2ecf2a279dfe936553
dinda <dinda.rachmat@gmail.com>
2018-03-13 18:27:20 +0700

    add printOperator variable to show operator (not 1-4 option) in the result

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,9 +1,26 @@
+/** Kalkulator
+
+ Nilai 1 : <inputA>			// 5
+ Nilai 2 : <inputB>			// 2
+ Pilih Operator:
+ 1. Tambah
+ 2. Kurang
+ 3. Kali
+ 4. Bagi
+ Pilihan (1..4) : <inputOperator>	// 1
+
+ 5 + 2 = 7
+ ...
+ Operator tidak ditemukan
+ **/
+
 import java.util.Scanner;
 
 public class Calculator {
     int number1;
     int number2;
     int operator;
+    String printOperator;
     double jawaban;
 
     public void inputCalculator() {
@@ -20,7 +37,7 @@ public class Calculator {
         System.out.println("3. Kali ");
         System.out.println("4. Bagi ");
 
-        System.out.print("Pilih Operator : " );
+        System.out.print("Pilih Operator(1-4): " );
         operator = scanner.nextInt();
 
         calculatorSwitchCase();
@@ -31,20 +48,26 @@ public class Calculator {
         switch (operator) {
             case 1:
                 jawaban = number1 + number2;
+                printOperator = "+";
                 break;
             case 2:
                 jawaban = number1 - number2;
+                printOperator = "-";
                 break;
             case 3:
                 jawaban = number1 * number2;
+                printOperator = "*";
                 break;
             case 4:
                 jawaban = number1 / number2;
+                printOperator = "/";
                 break;
+            default:
+                System.out.println("Operator tidak tersedia");
         }
     }
 
     public void resultCalculator(){
-        System.out.print(number1+" "+operator+" "+number2+" = "+jawaban);
+        System.out.print(number1+" "+printOperator+" "+number2+" = "+jawaban);
     }
 }
